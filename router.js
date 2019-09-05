d3cc905f22413bc1b193bb0389e73c11e6cdaca8
Funti <funtixa@gmail.com>
2019-09-05 20:31:38 +0200

    added author to post view

--- router.js
+++ router.js
@@ -10,11 +10,16 @@ router.post('/register', userController.register)
 router.post('/login',userController.login)
 router.post('/logout',userController.logout)
 
+//profile related routes
+
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostScreen)
+
 
 // post related post
 
 router.get('/create-post',userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
+
 
 module.exports = router
